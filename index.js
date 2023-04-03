1c1a6f9c4eddd9fd64b93a36b201fcbfd342f407
Elle Oshiro <ekoshiro16@gmail.com>
2023-04-02 22:21:24 -0700

    fixed typo

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ Where each string is a card from "Ace" to "King".
 
 Note: Remember that an in-place algorithm does NOT use any additional variables that use up more than constant space (i.e. O(1) space complexity). 
 Note 2: Remember that you must modify the original input value in an in-place algorithm. 
-Note 3: Try to aim for a time complexity that has a Big O analysis of O(n) (i.e. linear) complexity. 
 
 WARNING: Be mindful that each card should have an equal chance to any other card to be shuffled into a given random location. Avoid shuffling cards more than once! 
 */
