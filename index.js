281af77b019957293e170f5dc6a937fe333ace90
Elle Oshiro <ekoshiro16@gmail.com>
2023-04-02 22:19:55 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+Write an in-place algorithm that takes an array of strings (where the array represents a deck of cards, and each string represents the name of a given card in the deck).
+
+Your algorithm should return the input array, but with the cards shuffled in a random order. 
+
+For example, you could be provided an array like:
+['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+Where each string is a card from "Ace" to "King". 
+
+Note: Remember that an in-place algorithm does NOT use any additional variables that use up more than constant space (i.e. O(1) space complexity). 
+Note 2: Remember that you must modify the original input value in an in-place algorithm. 
+Note 3: Try to aim for a time complexity that has a Big O analysis of O(n) (i.e. linear) complexity. 
+
+WARNING: Be mindful that each card should have an equal chance to any other card to be shuffled into a given random location. Avoid shuffling cards more than once! 
+*/
+
+// Write your code below: 
